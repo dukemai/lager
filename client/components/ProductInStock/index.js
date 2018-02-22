70cc70f88ac17629cadd92fbf212dd04dc6fdfc8
duke mai <duc.mai@niteco.se>
2018-02-22 10:51:27 +0100

    Add form

--- client/components/ProductInStock/index.js
+++ client/components/ProductInStock/index.js
@@ -1,17 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Grid, Header, Breadcrumb, Form } from 'semantic-ui-react';
+
 import { AuthenticatedLayout } from '../share';
+
 
 const propTypes = {
 };
 const defaultProps = {
 };
 
-const ProductInStock = ({ }) => (
-  <AuthenticatedLayout>
-    ProductInStock
-  </AuthenticatedLayout>
-);
+const sections = [
+  {
+    key: 'inspect', content: 'Inspect', link: true, href: '/inspect',
+  },
+  { key: 'new-product', content: 'Add product', active: true },
+];
+
+class ProductInStock extends React.Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <AuthenticatedLayout>
+        <Grid padded>
+          <Grid.Row>
+            <Grid.Column textAlign="left" width="16">
+              <Breadcrumb sections={sections} />
+              <Header textAlign="left" as="h2">
+                Add new product to Stock
+              </Header>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column textAlign="left" width="10">
+              <Form>
+                <Form.Group widths="equal">
+                  <Form.Input fluid label="Product code" placeholder="Product code" />
+                  <Form.Input fluid label="Product name" placeholder="Product name" />
+                </Form.Group>
+                <Form.Input fluid label="Product category" placeholder="Product category" />
+                <Form.Group widths="equal">
+                  <Form.Input fluid label="Product price" placeholder="Product price" />
+                  <Form.Input fluid label="Product quantity" placeholder="Product quantity" />
+                </Form.Group>
+                <Form.Input fluid label="Product unit" placeholder="Product unit" />
+                <Form.Input fluid label="Date time" placeholder="Date time" />
+                <Form.Group widths="equal">
+                  <Form.Input fluid label="Contact name" placeholder="Contact name" />
+                </Form.Group>
+                <Form.Input fluid label="Phone number" placeholder="Phone number" />
+                <Form.Input fluid label="Email" placeholder="Email" />
+                <Form.Input fluid label="Address" placeholder="Address" />
+                <Form.Input fluid label="Company" placeholder="Company" />
+                <Form.Input fluid label="Tax" placeholder="Tax" />
+                <Form.Checkbox label="I agree to the Terms and Conditions" />
+                <Form.Button>Submit</Form.Button>
+              </Form>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </AuthenticatedLayout>
+    );
+  }
+}
+
 ProductInStock.propTypes = propTypes;
 ProductInStock.defaultProps = defaultProps;
 export default ProductInStock;
---
+++ client/components/share/NewProductForm/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Form, Accordion, Icon, Button, Divider } from 'semantic-ui-react';
+
+export default class NewProductForm extends Component {
+  state = { activeIndex: -1 }
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex } = this.state;
+    return (
+      <Form>
+        <Form.Group widths="equal">
+          <Form.Input fluid label="Product name" placeholder="Product name" />
+          <Form.Input fluid label="Product code" placeholder="Product code" />
+        </Form.Group>
+        <Form.Group widths="equal">
+          <Form.Input fluid label="Product price" placeholder="Product price" />
+          <Form.Input fluid label="Product quality" placeholder="Product quality" />
+        </Form.Group>
+        <Form.Field>
+          <Accordion>
+            <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+              <Icon name="dropdown" />
+              New Product
+            </Accordion.Title>
+            <Accordion.Content active={activeIndex === 0}>
+              <Form.Input fluid label="Product image" placeholder="Product image" />
+              <Form.Input fluid label="Product category" placeholder="Product category" />
+              <Form.Input fluid label="Producer" placeholder="Producer" />
+              <Form.Input fluid label="Distributor" placeholder="Distributor" />
+            </Accordion.Content>
+          </Accordion>
+        </Form.Field>
+        <Divider />
+        <Button positive>
+          Save
+        </Button>
+      </Form>
+    );
+  }
+}
